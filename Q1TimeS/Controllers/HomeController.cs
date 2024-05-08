0153d2c27411f1f717e65491167996f0d3397553
Wastalona <lenovoa516boy@gmail.com>
2024-05-09 01:23:53 +0300

    Created basic structure and made use case diagrams and activity diagrams

---
+++ Q1TimeS/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Q1TimeS.Models;
+using System.Diagnostics;
+
+namespace Q1TimeS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // General Views
+        public IActionResult SurveysPage()
+        /* Display of all surveys */
+        {
+            return View();
+        }        
+
+        public IActionResult SurveyInfoPage()
+        /* Display info about the selected survey */
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        /* Choice between administrator and user */
+        {
+            return View();
+        }
+
+        // Admin Views
+        public IActionResult CreateSurveyPage()
+        /* Page used to create a new survey */
+        {
+            return View();
+        }
+
+        public IActionResult SurveyPage()
+        /* Basic survey page used for the template */
+        {
+            return View();
+        }
+
+        public IActionResult AuthPage()
+        /* Administrator auth */
+        {
+            return View();
+        }
+        
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Q1TimeS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Q1TimeS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
