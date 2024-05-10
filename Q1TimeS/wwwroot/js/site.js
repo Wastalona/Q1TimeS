1a2b8d27f04ada18336ef4e5a4a224d1bf8a7a37
Wastalona <lenovoa516boy@gmail.com>
2024-05-10 22:07:00 +0300

    Updated styles for the index page and a few styles for the template. Also added db models and updated controllers. Testing on the release version.

--- Q1TimeS/wwwroot/js/site.js
+++ Q1TimeS/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var adminBlock = document.getElementById("admin-block");
+    var generalBlock = document.getElementById("general-block");
 
-// Write your JavaScript code.
+    adminBlock.addEventListener("click", function () {
+        window.location.href = "/admin/auth";
+    });
+
+    generalBlock.addEventListener("click", function () {
+        window.location.href = "/general/surveyspage";
+    });
+});
